b136622c8f076e13c436a1f33a96c8eb88561941
Nikhita <bnikhita2003@gmail.com>
2022-06-13 21:48:59 +0530

    modified main css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, Collectables } from "./pages";
 import Header from "./components/header/header.components";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collectables" element={<Collectables />} />
+      </Routes>
     </div>
   );
 }
