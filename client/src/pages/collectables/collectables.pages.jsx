1e4a473d2d801b99abd692b561775ac57e7b3f23
siddhantprateek <siddhantprateek@gmail.com>
2022-06-13 22:42:17 +0530

    collectables page fixed

--- client/src/pages/collectables/collectables.pages.jsx
+++ client/src/pages/collectables/collectables.pages.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './collectables.styles.css'
-import image from '../../assets/image 7.png'
+import React from "react";
+import "./collectables.styles.css";
 
 const Collectables = () => {
   return (
-    <div className='c-main'>
-      <div className="c-image">
-        <img src={image} alt="" />
-      </div>
-      <div className='c-content'>
-        <h1 className='c-heading'>Meet the Space Club Collectibles</h1>
-        <p className="c-para">A collections of over thousands unique Space themed NFTs — unique digital collectibles living on the Ethereum blockchain. Your Space theme doubles as your Space Club membership card, and grants access to members-only benefits, a collaborative graffiti board. Future areas and perks can be unlocked by the community through roadmap activation.</p>
+    <div className="c-main">
+      <div className="collectables-image-bg"></div>
+      <div className="collect-overlay"></div>
+
+      <div className="c-content">
+        <h1 className="c-heading">Meet the Space Club Collectibles</h1>
+        <p className="c-para">
+          A collections of over thousands unique Space themed NFTs — unique
+          digital collectibles living on the Ethereum blockchain. Your Space
+          theme doubles as your Space Club membership card, and grants access to
+          members-only benefits, a collaborative graffiti board. Future areas
+          and perks can be unlocked by the community through roadmap activation.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Collectables
+export default Collectables;
