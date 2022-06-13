29fcdb9becf1b9412fefe46deb6224dcefa9e86f
suvangipaul <suvangipaul8@gmail.com>
2022-06-13 19:12:45 +0530

    Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// import history from "./utils/history";
+// import { getConfig } from "./config";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const onRedirectCallback = (appState) => {
+//   history.push(
+//     appState && appState.returnTo ? appState.returnTo : window.location.pathname
+//   );
+// };
+
+// const config = getConfig();
+
+// const providerConfig = {
+//   domain: config.domain,
+//   clientId: config.clientId,
+//   ...(config.audience ? { audience: config.audience } : null),
+//   redirectUri: window.location.origin,
+//   onRedirectCallback,
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <Auth0Provider {...providerConfig}> */}
     <App />
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
