bfc27733122472d8bf73fcfa645ad9ba1950cf69
Nikhita <bnikhita2003@gmail.com>
2022-06-14 13:22:49 +0530

    modified

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header.components';
 export { default as Fundcard } from './fundcard/fundcard.components';
 export { default as Fundraiser } from './fundraisercard/fundraisercard.components';
+export { default as NFTCards } from './nftcards/nftcards.components';
